3833bbab34abd93545ec2cb60aa5fe68f1644df4
goforit615 <bupark82@gsm.hs.kr>
2025-03-24 17:38:44 +0900

    initial commit

---
+++ ThirdAssignment/src/MedalBag.java
@@ -0,0 +1,26 @@
+/* 메서드
+    아래의 medal_bag에는 광주 소프트웨어마이스터고등학교 학생들이 대회에 참여해 얻은 메달의 종류들이 뒤죽박죽 담겨있다.
+    이것을 메달 별로 몇 개가 있는지 카운트해주는 count_individual 메서드와 메달 별로 카운트된 것을 배열로 만들어 주는
+    make_array 메서드가 있다. pass를 지운 후 출력 결과와 동일하게 출력하는 코드를 작성해 주시고 출력 결과와 동일한 출력을 해주세요.
+*/
+
+// 주어진 count_individual(), make_array() 직접 수정 불가
+// 출력 결과 :
+// Gold: 6
+// Silver: 4
+// Bronze: 3
+public class MedalBag {
+    public static void main(String[] args) {
+        String[] medal_bag = {"Gold", "Silver", "Gold", "Bronze", "Gold", "Silver", "Bronze", "Gold", "Silver", "Gold", "Silver", "Bronze", "Gold", "Silver", "Bronze"};
+        count_individual(medal_bag);
+        make_array(medal_bag);
+    }
+
+    public static void count_individual(String[] medal_bag) {
+        //pass
+    }
+
+    public static void make_array(String[] medal_bag) {
+        //pass
+    }
+}
